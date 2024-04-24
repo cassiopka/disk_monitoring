7942ee6d5feab7a0c88c82294801870321f1f0d3
cassiopelka <cassiopelka@githubc.com>
2024-04-24 03:37:08 +0300

    log update

--- disk_monitoring.sh
+++ disk_monitoring.sh
@@ -5,12 +5,16 @@ LOG_FILE="/var/log/disk_monitoring.log"
 
 # Получение информации о диске
 USAGE=$(df -h / | awk 'NR==2{print $5}' | sed 's/%//')
+USED=$(df -h / | awk 'NR==2{print $3}')
+TOTAL=$(df -h / | awk 'NR==2{print $2}')
 
 # Проверка использования диска
 if [ $USAGE -gt 80 ]; then
     # Запись предупреждения в файл журнала
-    echo "Внимание! Использование диска превышает 80%: $USAGE%" | tee -a $LOG_FILE
+    echo "$(date) [WARNING] Disk usage exceeds 80%: $USAGE% ($USED of $TOTAL) on $(hostname)" | sudo tee -a $LOG_FILE
+    # Отправка уведомления по электронной почте
+    echo "Disk usage warning on $(hostname): $USAGE% ($USED of $TOTAL)" | mail -s "Disk usage warning" user@example.com
 else
     # Запись сообщения в файл журнала
-    echo "Уровень использования диска в пределах нормы: $USAGE%" | tee -a $LOG_FILE
+    echo "$(date) [INFO] Disk usage is within normal range: $USAGE% ($USED of $TOTAL) on $(hostname)" | sudo tee -a $LOG_FILE
 fi
